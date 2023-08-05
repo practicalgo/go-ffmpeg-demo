e92d8740c03bdaf739756594a6f17c46f88ae1b1
Amit Saha <mail@echorand.me>
2023-08-06 09:06:37 +1000

    failing test

---
+++ thumbnail.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"os/exec"
+	"time"
+)
+
+func createThumbnail(
+	cmdCtx context.Context,
+	inputData []byte,
+	cmdTimeout time.Duration,
+) ([]byte, error) {
+
+	var cmdOut, cmdErr bytes.Buffer
+	var err error
+
+	ch := make(chan error, 1)
+
+	ctx, cancel := context.WithTimeout(cmdCtx, cmdTimeout)
+	defer cancel()
+
+	// ffmpeg will work with stdin and stdout
+	// we set those up next
+	cmd := exec.CommandContext(
+		ctx,
+		"ffmpeg", "-i", "pipe:0", "-vf", "scale='iw/2:ih/2'", "-f", "image2", "pipe:1",
+	)
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	cmd.Stdout = &cmdOut
+	cmd.Stderr = &cmdErr
+
+	go func() {
+		defer stdin.Close()
+		log.Println("writing input data to pipe..")
+		_, err = io.WriteString(stdin, string(inputData))
+	}()
+
+	go func() {
+
+		// we cannot use CombinedOutput() here as it returns both
+		// stdout and stderr
+		// stdout will contain our transformed image when successful
+		// we also want to report the entire error when there is a problem
+		// so, we have to separate out stdout and stderr explicitly
+		//out, err := cmd.CombinedOutput()
+
+		log.Println("creating thumbnail of a specified image using ffmpeg..")
+		err = cmd.Run()
+		if err != nil {
+			log.Println(string(cmdErr.Bytes()))
+		}
+		ch <- err
+	}()
+
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+	case err = <-ch:
+	}
+
+	return cmdOut.Bytes(), err
+}
---
+++ thumbnail_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestCreateThumbnail(t *testing.T) {
+	originalImage, err := os.ReadFile("testdata/book_cover.jpg")
+	if err != nil {
+		t.Fatal("error reading test file", err)
+	}
+
+	thumbnailImage, err := createThumbnail(context.Background(), originalImage, 1*time.Second)
+	if err != nil {
+		t.Fatal("error creating thumbnail", err)
+	}
+	// TODO assert the width and height of the image
+	if len(thumbnailImage) == 0 || len(thumbnailImage) >= len(originalImage) {
+		t.Fatal("thumbnail not created successfully")
+	}
+
+}
