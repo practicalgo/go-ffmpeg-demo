3097d828694b9586ccf77366d3ac59b5bf9fbebe
Amit Saha <mail@echorand.me>
2023-08-06 11:12:24 +1000

    use temp files instead of pipes

--- thumbnail.go
+++ thumbnail.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"log"
+	"os"
 	"os/exec"
+	"path"
 	"time"
 )
 
@@ -15,46 +16,50 @@ func createThumbnail(
 	cmdTimeout time.Duration,
 ) ([]byte, error) {
 
-	var cmdOut, cmdErr bytes.Buffer
 	var err error
 
 	ch := make(chan error, 1)
+	// TODO image id
+	tmpDir, err := os.MkdirTemp("", "thumbnail-")
+	if err != nil {
+		log.Println("error creating temporary directory", err)
+		return nil, err
+	}
+	defer func() {
+		err := os.RemoveAll(tmpDir)
+		if err != nil {
+			log.Println("error cleaning up", tmpDir)
+		}
+	}()
+
+	inputFile, err := os.CreateTemp(tmpDir, "input-image")
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.WriteString(inputFile, string(inputData))
+	if err != nil {
+		return nil, err
+	}
 
 	ctx, cancel := context.WithTimeout(cmdCtx, cmdTimeout)
 	defer cancel()
 
 	// ffmpeg will work with stdin and stdout
 	// we set those up next
+	outputFile := path.Join(tmpDir, "thumbnail-image-id")
 	cmd := exec.CommandContext(
 		ctx,
-		"ffmpeg", "-i", "pipe:0", "-vf", "scale='iw/2:ih/2'", "-f", "image2", "pipe:1",
+		"ffmpeg", "-i", inputFile.Name(), "-vf", "scale='iw/2:ih/2'", "-f", "image2", outputFile,
 	)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return nil, err
-	}
-	cmd.Stdout = &cmdOut
-	cmd.Stderr = &cmdErr
 
 	go func() {
-		defer stdin.Close()
-		_, err = io.WriteString(stdin, string(inputData))
-	}()
-
-	go func() {
-
-		// we cannot use CombinedOutput() here as it returns both
-		// stdout and stderr
-		// stdout will contain our transformed image when successful
-		// we also want to report the entire error when there is a problem
-		// so, we have to separate out stdout and stderr explicitly
-		//out, err := cmd.CombinedOutput()
 
 		log.Println("creating thumbnail of a specified image using ffmpeg..")
-		err = cmd.Run()
+		output, err := cmd.CombinedOutput()
 		if err != nil {
-			log.Println(string(cmdErr.Bytes()))
+			log.Println("error creating thumbnail", string(output))
 		}
+
 		ch <- err
 	}()
 
@@ -64,5 +69,15 @@ func createThumbnail(
 	case err = <-ch:
 	}
 
-	return cmdOut.Bytes(), err
+	if err != nil {
+		return nil, err
+	}
+
+	thumbnailBytes, err := os.ReadFile(outputFile)
+	if err != nil {
+		log.Println("error reading from output file", outputFile)
+		return nil, err
+	}
+
+	return thumbnailBytes, err
 }
